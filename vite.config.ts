e36186094b4ee4394e0fdf479eb7b935022e4ccb
jaysprogram <107363449+jaysprogram@users.noreply.github.com>
2026-04-29 16:38:51 -0400

    update config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['raetec.onrender.com', 'ratec.net'],
+    allowedHosts: ['raetec.onrender.com', 'raetec.net'],
   },
 })
